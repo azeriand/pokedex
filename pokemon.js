1b7e155a17330f86357b49af525aaf9f2d7013ef
azeriand <andrearomeracosta4@gmail.com>
2024-12-01 19:54:16 +0100

    Filter gen by id

--- pokemon.js
+++ pokemon.js
@@ -1,3 +1,26 @@
+
+function GuessGen(number){
+    if (number > "898")
+        return "9"
+    else if (number > "809")
+        return "8"
+    else if (number > "721")
+        return "7"
+    else if (number > "649")
+        return "6"
+    else if (number > "493")
+        return "5"
+    else if (number > "386")
+        return "4"
+    else if (number > "251")
+        return "3"
+    else if (number > "151")
+        return "2"
+    else if (number > "0")
+        return "1"
+}
+
+
 class Pokemon {
     constructor(name, number, type, type2, gen, description, sprite){
         this.name = name
